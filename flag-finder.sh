a0ccf9ca47384af6f5595ac38b98bc8df38a30b8
migueltc13 <migueltc13@gmail.com>
2023-10-07 18:37:49 +0100

    Update flag-finder.sh

--- flag-finder.sh
+++ flag-finder.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-echo "Finding flags..."
-
 # Find by name
-find / -type f -name "*flag.txt" -o -name ".flag*" -o -name "flag" -o -name "user.txt" -o -name "root.txt" 2>/dev/null > temp_flags.txt
+find / -type f -name "*flag.txt" -o -name ".flag*" -o -name "flag" -o -name "user.txt" -o -name "root.txt" 2>/dev/null
 
 # Find by date (use stat to check file dates)
-find / -type f -newermt "2021-02-15 00:00:00" ! -newermt "2021-02-16 00:00:00" -ls 2>/dev/null >> temp_flags.txt
+find / -type f -newermt "2021-02-15 00:00:00" ! -newermt "2021-02-16 00:00:00" -ls 2>/dev/null
 
 # Find by content
-# grep -R "THM{" / 2>/dev/null >> temp_flags.txt
-# grep -R "VEhN" / 2>/dev/null >> temp_flags.txt # base64
-
-if [[ -s temp_flags.txt ]]; then
-    echo "All flags found"
-    echo "Here are your flags."
-    cat temp_flags.txt
-else
-    echo "No flags found"
-fi
+# grep -iR "THM{" / 2>/dev/null
+# grep -R  "VEhN" / 2>/dev/null # base64
