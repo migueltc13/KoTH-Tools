d3e3fdc70cb8508afb0c37064704596a689d294f
migueltc13 <migueltc13@gmail.com>
2023-10-14 23:13:37 +0100

    Update chattr flag-finder.sh

--- flag-finder.sh
+++ flag-finder.sh
@@ -7,5 +7,6 @@ find / -type f -name "*flag.txt" -o -name ".flag*" -o -name "flag" -o -name "use
 find / -type f -newermt "2021-02-15 00:00:00" ! -newermt "2021-02-16 00:00:00" -ls 2>/dev/null
 
 # Find by content
-# grep -iR "THM{" / 2>/dev/null
-# grep -R  "VEhN" / 2>/dev/null # base64
+# grep -iR "THM{" / 2>/dev/null          # flag prefix
+# grep -R  "VEhN" / 2>/dev/null          # base64
+# grep -RE '[0-9a-f]{32}' . 2>/dev/null  # hash
