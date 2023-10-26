64a6c0dd8e097758e644ba3eeccf8bc37a6c5bd8
migueltc13 <migueltc13@gmail.com>
2023-10-26 12:29:41 +0100

    Get local files paths

--- url-path-finder.sh
+++ url-path-finder.sh
@@ -1,1 +1,19 @@
-export URL=www.inlanefreight.com; export PROTOCOL=https; curl $PROTOCOL://$URL | tr " " "\n" | cut -d"'" -f2 | cut -d'"' -f2 | grep "$URL" | sort -u
+#!/bin/bash
+
+URL=$1
+PROTOCOL=$2
+
+if [ "$1" == "" ]; then
+    echo "Usage: $0 <URL> [PROTOCOL]"
+fi
+
+if [ "$2" == "" ]; then
+    PROTOCOL="http"
+fi
+
+curl "$PROTOCOL://$URL" | tr " " "\n" | cut -d"'" -f2 | cut -d'"' -f2 | grep "$URL" | sort -u
+
+# TODO: check if this works to get local links
+curl "$PROTOCOL://$URL" | grep "href=/" | sort -u
+
+exit 0
