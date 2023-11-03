15e01840cf4dc6a5c8c1d9bb7a546577f18689f0
migueltc13 <migueltc13@gmail.com>
2023-11-03 19:08:29 +0000

    Update url-path-finder.sh

--- url-path-finder.sh
+++ url-path-finder.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-URL=$1
-PROTOCOL=$2
-
 if [ "$1" == "" ]; then
-    echo "Usage: $0 <URL> [PROTOCOL]"
+    echo "Usage: $0 <URL>"
+    exit 1
 fi
 
-if [ "$2" == "" ]; then
-    PROTOCOL="http"
-fi
+URL=$1
 
-curl "$PROTOCOL://$URL" | tr " " "\n" | cut -d"'" -f2 | cut -d'"' -f2 | grep "$URL" | sort -u
-
-# TODO: check if this works to get local links
-curl "$PROTOCOL://$URL" | grep "href=/" | sort -u
+# TODO only get the links in the command bellow
+curl -s "$URL" | grep "$URL" | sort -u
+curl -s "$URL" | grep -Eo "href=\".*\"" | cut -d'"' -f2 | sort -u
+# curl -s "$URL" | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*" | sort -u
 
 exit 0
