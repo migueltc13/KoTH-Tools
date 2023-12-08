11190c03c252f55c21c00d9a71d2352c779d9b60
migueltc13 <migueltc13@gmail.com>
2023-12-08 20:00:37 +0000

    Create rickroll.sh :)

---
+++ animations/rickroll.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script will print the lyrics to Never Gonna Give You Up
+
+ST=1  # Sleep Time (seconds)
+sleep $ST; echo "We're no strangers to love"
+sleep $ST; echo "You know the rules and so do I"
+sleep $ST; echo "A full commitment's what I'm thinking of"
+sleep $ST; echo "You wouldn't get this from any other guy"
+sleep $ST; echo "I just wanna tell you how I'm feeling"
+sleep $ST; echo "Gotta make you understand"
+sleep $ST; echo "Never gonna give you up"
+sleep $ST; echo "Never gonna let you down"
+sleep $ST; echo "Never gonna run around and desert you"
+sleep $ST; echo "Never gonna make you cry"
+sleep $ST; echo "Never gonna say goodbye"
+sleep $ST; echo "Never gonna tell a lie and hurt you"
+sleep $ST; echo "We've known each other for so long"
+sleep $ST; echo "Your heart's been aching but you're too shy to say it"
+sleep $ST; echo "Inside we both know what's been going on"
+sleep $ST; echo "We know the game and we're gonna play it"
+sleep $ST; echo "And if you ask me how I'm feeling"
+sleep $ST; echo "Don't tell me you're too blind to see"
+sleep $ST; echo "Never gonna give you up"
+sleep $ST; echo "Never gonna let you down"
+sleep $ST; echo "Never gonna run around and desert you"
+sleep $ST; echo "Never gonna make you cry"
+sleep $ST; echo "Never gonna say goodbye"
+sleep $ST; echo "Never gonna tell a lie and hurt you"
+sleep $ST; echo "Never gonna give you up"
+
+exit 0
